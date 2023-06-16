0f6aa468e1e7dec8733626b84d0018275899035e
Gerardo <gcerpa02@cox.net>
2023-06-16 09:09:13 -0700

    Created base project

---
+++ project/carhub/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World from React!</h1>
+      <button className="custom-button">Custom Button</button>
+      <button className="btn btn-primary">Custom Button</button>
+    </div>
+  );
+}
+
+export default App;
