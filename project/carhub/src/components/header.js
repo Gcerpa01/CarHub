c9b2b1fe468a4cc020e05e884c340ae70723e32a
Jarrod Rowson <jrowson7843@sdsu.edu>
2023-06-18 23:04:33 -0700

    Header / Footer Update 06181104 - Logo

--- project/carhub/src/components/header.js
+++ project/carhub/src/components/header.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import './Header.css'; // Assuming the CSS file is named "Header.css"
+import logo from '../resources/logo.png';
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="top-bar">
-        <div className="account-dropdown">
-          <button className="account-button">Account</button>
-          <div className="dropdown-content">
-            {/* Dropdown content here */}
-          </div>
-        </div>
-      </div>
       <nav className="navbar">
-        <div className="logo">
-          <a href="/">LOGO HERE</a>
-        </div>
+        <a href="/">
+        <img src={logo} alt="Logo" />
+        </a>
         <ul className="nav-links">
-          <li><a href="/">Home</a></li>
           <li><a href="/groups">Groups</a></li>
           <li><a href="/messages">Messages</a></li>
           <li><a href="/shop">Shop</a></li>
           <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+      <nav className="actionbar">
+        <ul className="actionbar-links">
+          <li><a href="/">My Account</a></li>
+          <li><a href="/">Cart ( {/* script goes here */} )</a></li>
         </ul>
       </nav>
     </header>
