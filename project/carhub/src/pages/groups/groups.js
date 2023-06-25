068056d2c00dc223604e7399f2809d7b2c1fc76f
Gerardo <gcerpa02@cox.net>
2023-06-24 18:18:20 -0700

    base front-end finished for groups

--- project/carhub/src/pages/groups/groups.js
+++ project/carhub/src/pages/groups/groups.js
@@ -1,11 +1,25 @@
 import Header from '../../components/header'
 import Footer from '../../components/footer'
+import Carousel from 'bootstrap/js/dist/carousel'; // Import the Carousel component from Bootstrap
+
 import './Groups.css'; // Assuming the CSS file is named "Header.css"
 
 import groupData from '../../db/groupData.json';
 import { isVisible } from '@testing-library/user-event/dist/utils';
+import { useEffect } from 'react'; // Import the useEffect hook
+
 
 export default function Groups() {
+
+
+    useEffect(() => {
+        // Initialize the carousel after the component has mounted
+        var carousel = new Carousel(document.getElementById('carousel-Groups'), {
+            interval: 5000,
+        });
+    }, []);
+
+
     return (
         <>
             <Header />
@@ -17,7 +31,7 @@ export default function Groups() {
                     </div>
                 </form>
 
-                <div className="search-container" style={{visibility:"visible"}}>
+                <div className="search-container" style={{ visibility: "visible" }}>
                     {/* Generate search results dynamically */}
                     {groupData.groups.map((group, index) => (
                         <div className="search-result-container" key={index}>
@@ -33,50 +47,67 @@ export default function Groups() {
 
             </div>
 
-
-
-            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active" data-bs-interval="10000">
-                        <img src="https://www.shutterstock.com/image-photo/motorbike-on-road-riding-having-260nw-1838812744.jpg" class="rounded float-left w-50" />
-                        <div class="carousel-container">
-                            <div class="carousel-caption custom-caption">
-                                <h2 class="d-flex h-25 align-items-center justify-content-center">Motorcycle Hobbiests</h2>
-                                <p class="d-flex h-50 align-items-center justify-content-center">This contains the information regarding the current club</p>
-                                <p><a class="btn btn-sm btn-primary align-items-center" href="#">Join Group</a> </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item" data-bs-interval="2000">
-                        <img src="https://www.shutterstock.com/image-photo/motorbike-on-road-riding-having-260nw-1838812744.jpg" class="d-block w-50" alt="slide1" />
-                        <div class="container">
-                            <div class="carousel-caption">
-                                <h1>Motorcycle Hobbiests2</h1>
-                                <p> This contains the information regarding the current club</p>
-                                <p><a class="btn btn-large btn-primary" href="#">Join Group</a> </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="https://www.shutterstock.com/image-photo/motorbike-on-road-riding-having-260nw-1838812744.jpg" class="d-block w-50" alt="slide1" />
-                        <div class="container">
-                            <div class="carousel-caption">
-                                <h1>Motorcycle Hobbiests3</h1>
-                                <p> This contains the information regarding the current club</p>
-                                <p><a class="btn btn-large btn-primary" href="#">Join Group</a> </p>
-                            </div>
-                        </div>
-                    </div>
+            <div id="carousel-Groups" class="carousel" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carousel-Groups" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carousel-Groups" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carousel-Groups" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                <div class="carousel-inner">
+
+                    <div class="carousel-item active" >
+                        <img src="https://www.shutterstock.com/image-photo/motorbike-on-road-riding-having-260nw-1838812744.jpg" class="group-image" />
+                        <div className="custom-caption">
+                            <h2 className="carousel-group-name">Motorcycle Hobbiests</h2>
+                            <h3 className="carousel-group-members"> members: 6</h3>
+                            <div className="carousel-description">
+                                <p>This contains the test nformation regarding the current club </p>
+                            </div>
+                            <p><a class="btn btn-sm btn-primary align-items-center" href="#">Join Group</a> </p>
+                        </div>
+                    </div>
+                    
+
+                    <div class="carousel-item " >
+                        <img src="https://www.shutterstock.com/image-photo/motorbike-on-road-riding-having-260nw-1838812744.jpg" class="group-image" />
+                        <div className="custom-caption">
+                            <h2 className="carousel-group-name">Motorcycle Hobbiests</h2>
+                            <h3 className="carousel-group-members"> members: 6</h3>
+                            <div className="carousel-description">
+                                <p>This contains the test nformation regarding the current club </p>
+                            </div>
+                            <p><a class="btn btn-sm btn-primary align-items-center" href="#">Join Group</a> </p>
+                        </div>
+                    </div>
+                    
+
+                    <div class="carousel-item ">
+                        <img src="https://www.shutterstock.com/image-photo/motorbike-on-road-riding-having-260nw-1838812744.jpg" class="group-image" />
+                        <div className="custom-caption">
+                            <h2 className="carousel-group-name">Motorcycle Hobbiests</h2>
+                            <h3 className="carousel-group-members"> members: 6</h3>
+                            <div className="carousel-description">
+                                <p>This contains the test nformation regarding the current club </p>
+                            </div>
+                            <p><a class="btn btn-sm btn-primary align-items-center" href="#">Join Group</a> </p>
+                        </div>
+                    </div>
+                    
+
+                </div>
+
+                <button class="carousel-control-prev " type="button" data-bs-target="#carousel-Groups" data-bs-slide="prev" >
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+
+                <button class="carousel-control-next" type="button" data-bs-target="#carousel-Groups" data-bs-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
                 </button>
             </div>
+
+
             <Footer />
         </>
     )
