073164a4f9109fda6a00baa5481513ca550da00f
Jacob Hosburg <jhosburg22@gmail.com>
2023-06-25 17:12:14 -0700

    Completed core components for main shop page, added exhaust.js

---
+++ project/carhub/src/pages/exhaust.js
@@ -0,0 +1,12 @@
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+export default function Exhaust() {
+  return (
+    <div className="App">
+      <Header />
+      
+      <Footer />
+    </div>
+  );
+}
