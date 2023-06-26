45fee7a5c1764086e57c69e77e6ecc950f2eafb1
Jacob Hosburg <jhosburg22@gmail.com>
2023-06-25 20:04:26 -0700

    style changes and updated ui with review section

--- project/carhub/src/pages/exhaust.js
+++ project/carhub/src/pages/exhaust.js
@@ -5,6 +5,8 @@ export default function Exhaust() {
   return (
     <div className="App">
       <Header />
+      <h2>Exhaust Systems and Components</h2>
+      <br></br>
       
       <Footer />
     </div>
