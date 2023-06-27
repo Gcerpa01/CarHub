ec5954181bc1317210e2f670f7fca4840d9dfc2c
LeoRoacho <leoroacho@gmail.com>
2023-06-26 22:21:02 -0700

    fixing itemshop errors

--- project/carhub/src/pages/messages/messages.js
+++ project/carhub/src/pages/messages/messages.js
@@ -1,68 +1,102 @@
-import Header from '../components/header'
-import Footer from '../components/footer'
-import './Shop.css';
+import Header from '../../components/header'
+import Footer from '../../components/footer'
+import './Messages.css'; // Assuming the CSS file is named "Header.css"
+import React, { useState, useEffect } from 'react';
+import groupsData from '../../db/subscribed_groups.json';
 
-export default function ItemShop() {
+export default function Messages() {
+  const groups = groupsData.groups;
+
+  const [selectedOption, setSelectedOption] = useState(localStorage.getItem('selectedOption') || groups[0].name);
+
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    localStorage.setItem('selectedOption', option);
+  };
+
+  const renderOptionContent = () => {
+    switch (selectedOption) {
+      case 'Ford':
+        return (
+          <div className="message-logs-container">
+            <div className="list-group">
+              <li className="message-group-item" > <text className="User"> User 1: </text> Cras justo odio testing the amount of lines that will</li>
+              <li className="message-group-item"> <text className="User"> User 1: </text> Dapibus ac facilisis in</li>
+              <li className="message-group-item">  <text className="Main-User"> User 2: </text> Morbi leo risus</li>
+              <li className="message-group-item">  <text className="Main-User"> User 2: </text> Porta ac consectetur ac</li>
+              <li className="message-group-item"> <text className="User"> User 1: </text>Vestibulum at eros</li>
+              <li className="message-group-item">  <text className="Main-User"> User 2: </text> Vestibulum at eros</li>
+              <li className="message-group-item"> <text className="User"> User 1: </text>Vestibulum at eros</li>
+            </div>
+          </div>
+        );
+      case 'Kia':
+        return (
+          <div className="message-logs-container">
+            <div className="list-group">
+              <li className="message-group-item" > <text className="User"> User 1: </text> Cras justo odio testing the amount of lines that will</li>
+              <li className="message-group-item"> <text className="User"> User 1: </text> Dapibus ac facilisis in</li>
+              <li className="message-group-item">  <text className="Main-User"> User 2: </text> Morbi leo risus</li>
+            </div>
+          </div>
+        );
+      case 'Tesla':
+        return (
+          <div className="message-logs-container">
+            <div className="list-group">
+              <li className="message-group-item" > <text className="User"> User 1: </text> Cras justo odio testing the amount of lines that will</li>
+            </div>
+          </div>
+        );
+      default:
+        return (
+          <div className="message-logs-container">
+            <div className="list-group">
+              <li className="message-group-item" > <text className="User"> User 1: </text> Cras justo odio testing the amount of lines that will</li>
+              <li className="message-group-item"> <text className="User"> User 1: </text> Dapibus ac facilisis in</li>
+              <li className="message-group-item">  <text className="Main-User"> User 2: </text> Morbi leo risus</li>
+              <li className="message-group-item">  <text className="Main-User"> User 2: </text> Porta ac consectetur ac</li>
+              <li className="message-group-item"> <text className="User"> User 1: </text>Vestibulum at eros</li>
+              <li className="message-group-item">  <text className="Main-User"> User 2: </text> Vestibulum at eros</li>
+              <li className="message-group-item"> <text className="User"> User 1: </text>Vestibulum at eros</li>
+            </div>
+          </div>
+        );
+    }
+  };
+
   return (
     <div className="App">
-      <Header />
-      <div className="float-child">
-        <div className="item-img"><img src="BMW-Exhaust.jpg" class ="rounded" width ="500" height ="500"/></div>
-      </div>
-      <div className="float-child">
-        <div>
-          <h2 className="item-name">Bomiz Full Titanium Valvetronic Exhaust for BMW E9x M3<br></br></h2>
-          <p className="item-brand">Bomiz</p>
-          <p className="item-brand">Part Number: (CH-FTX9)</p>
-          <p className="item-price">$5,800</p>
-          <div>
-            <button className="quantity">-</button>
-            <p className="quantity">Quantity: 1</p>
-            <button className="quantity">+</button>
-            <button className="add-to-cart">Add to Cart</button>
+      <div className="messages-page">
+        <Header />
+        <div className="messages-container">
+
+          {/* Making Groups on left side */}
+          <div className="groups-container">
+
+            {groupsData.groups.map((group, index) => (
+              <button
+                key={index}
+                className={selectedOption === group.name ? 'active' : ''}
+                onClick={() => handleOptionClick(group.name)}
+              >
+                {group.name}
+              </button>
+            ))}
+
+
           </div>
-          <div>
-          <h3 className="features-title">Description</h3>
-          <div className="description">
-              <p>
-              If you’re looking for an exhaust that you can throw on, make your car sound insanely loud or quiet at anytime you want,
-               along with giving it a nice look between all the exhaust tips you can choose from, 
-               this is a solid setup for your car.
-              </p>
-            </div>
-          </div>
-          <div>
-            <h3 className="features-title">Features</h3>
-            <div className="features">
-              <li>Weight Savings</li>
-              <li>Aggressive Sound</li>
-              <li>Higher Quality and Stronger Material</li>
-              <li>Tips made from Titanium</li>
-            </div>
-          </div>
-          <br></br>
-          <div>
-            <h3 className="features-title">Specifications</h3>
-            <div className="features">
-              <li>Built from Titanium, including all components</li>
-              <li>This is a Full System, Not an Axle-Back</li>
-              <li>High Quality Welding</li>
-              <li>Adjustable 89mm Single Wall Tips w/ Bomiz Logo</li>
-              <li>Electronic Valves </li>
-              <li>HP and Torque Increase</li>
-            </div>
-          </div>
-          <br></br>
-          <div>
-            <h3 className="features-title">What's Included</h3>
-            <div className="features">
-              <li>Rear Mufflers + Connecting Pipes</li>
-              <li>X Pipe + Test Pipes</li>
-              <li>2x Remote Controls + Receiver</li>
-              <li>4x 89mm Tips</li>
-              <li>Necessary Hardware and Clamps </li>
-              <li>Installation Instructions</li>
-            </div>
+
+          <div className="headerDivider"></div>
+
+          <div className="dms-container">
+            {renderOptionContent()}
+            <form>
+              <div>
+                <input type="text" id="message_typed" className="form-control" placeholder="Type your message:" />
+              </div>
+            </form>
           </div>
         </div>
       </div>
