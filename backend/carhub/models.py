79ac9ad7bb6da0756a8805ceb1020807d622c748
Gerardo <gcerpa02@cox.net>
2023-06-28 01:11:36 -0700

    fixing changes

--- backend/carhub/models.py
+++ backend/carhub/models.py
@@ -6,6 +6,9 @@ class Group(models.Model):
     description = models.CharField(max_length=500)
     joined = models.BooleanField()
     image = models.URLField(max_length=200)
+    def __str__(self):
+        return self.name
+
 
 class Profile(models.Model):
     name = models.CharField(max_length=200)
