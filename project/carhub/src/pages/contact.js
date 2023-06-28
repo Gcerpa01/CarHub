302e192760c4844be80a99dd6d4a1aa709783610
Leonardo Roacho <134570589+LeoRoacho@users.noreply.github.com>
2023-06-28 00:19:42 -0400

    Update contact.js

--- project/carhub/src/pages/contact.js
+++ project/carhub/src/pages/contact.js
@@ -7,30 +7,30 @@ export default function Contact() {
       <Header />
       <h2>Contact Page</h2>
       <br/>
-      <div class="row">
-        <div class= "col"></div>
-        <div class="col align-self-center">
+      <div className="row">
+        <div className= "col"></div>
+        <div className="col align-self-center">
           <form>
-            <div class="form-group">
-              <label for="senderName">Please enter your name:</label>
-              <input type="text" class="form-control" id="senderName" placeholder="Name"></input>
+            <div className="form-group">
+              <label htmlFor="senderName">Please enter your name:</label>
+              <input type="text" className="form-control" id="senderName" placeholder="Name"></input>
             </div>
             <br/>
-            <div class="form-group">
-              <label for="userEmail">Please enter your email address:</label>
-              <input type="email" class="form-control" id="userEmail" aria-describedby="emailHelp" placeholder="Enter email"></input>
-              <small id="emailHelp" class="form-text text-muted">We'll never share your email with the NSA.</small>
+            <div className="form-group">
+              <label htmlFor="userEmail">Please enter your email address:</label>
+              <input type="email" className="form-control" id="userEmail" aria-describedby="emailHelp" placeholder="Enter email"></input>
+              <small id="emailHelp" className="form-text text-muted">We'll never share your email with the NSA.</small>
             </div>
             <br/>
-            <div class="form-group">
-              <label for="message">Please enter your message:</label>
-              <textarea class="form-control" id="message" rows="4"></textarea>
+            <div className="form-group">
+              <label htmlFor="message">Please enter your message:</label>
+              <textarea className="form-control" id="message" rows="4"></textarea>
             </div>
             <br/>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">Submit</button>
           </form>
         </div>
-        <div class="col"></div>
+        <div className="col"></div>
       </div>
       <Footer />
     </div>
