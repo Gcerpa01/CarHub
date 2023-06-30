1df03f48985dfb4bbe8fe615702fb7e77ce35f92
Gerardo <gcerpa02@cox.net>
2023-06-30 00:31:32 -0700

    Run as user to run website from single script

---
+++ Run_as_user.py
@@ -0,0 +1,50 @@
+import subprocess
+import os
+import sys
+from threading import Thread
+import time
+
+def run_npm_module(selected_dir):
+    try:
+        subprocess.check_call(['npm', 'start'], cwd=selected_dir)
+        print(f"Front end started successfully")
+    except subprocess.CalledProcessError:
+        print(f"Front end failed to start")
+
+def run_in_virtualenv(command, selected_dir):
+    try:
+        activate_cmd = 'source .venv/bin/activate && '
+        full_cmd = activate_cmd + command
+        subprocess.check_call(full_cmd, shell=True, cwd=selected_dir)
+    except subprocess.CalledProcessError:
+        print("Failed to execute command in the virtual environment.")
+
+
+print("---------------------------------")
+print("---------------------------------")
+print("Force quit will be required to exit: CNTRL + C")
+print("---------------------------------")
+print("---------------------------------")
+
+time.sleep(5)
+
+current_file_path = os.path.abspath(__file__)  # project file destination
+front_file_path = os.path.join(os.path.dirname(current_file_path), 'project/carhub/src')
+back_file_path = os.path.join(os.path.dirname(current_file_path), 'backend')
+
+run_front_end = Thread(target=run_npm_module, args=(front_file_path,))
+run_back_end = Thread(target=run_in_virtualenv, args=('python3 manage.py runserver', back_file_path))
+
+run_front_end.start()
+run_back_end.start()
+
+time.sleep(10)
+
+print("---------------------------------")
+print("---------------------------------")
+print("Force quit required to exit: CNTRL + C")
+print("---------------------------------")
+print("---------------------------------")
+
+run_front_end.join()
+run_back_end.join()
