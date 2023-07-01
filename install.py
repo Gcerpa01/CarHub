a81d919942e4b6b3553fa515ce72599449d1c998
Gerardo <gcerpa02@cox.net>
2023-06-30 18:07:15 -0700

    added nodejs check for install.py

--- install.py
+++ install.py
@@ -2,14 +2,27 @@
 import subprocess
 import os
 import sys
+import time
+def clear_output():
+    if sys.platform.startswith('win'):  # For Windows
+        os.system('cls')
+    else:  # For Linux and Mac
+        os.system('clear')
 
 
-def install_npm_module(module_name,selected_dir):
+def install_npm_module(module_name,selected_dir,save=False):
     try:
-        subprocess.check_call(['npm', 'install', module_name], cwd=selected_dir)
-        print(f"Successfully installed npm module: {module_name}")
+        npm_commands = ['npm', 'install', module_name]
+        if save:
+            npm_commands.append("--save")
+        subprocess.check_call(npm_commands, cwd=selected_dir)
+        print(f"\n***Successfully installed npm module: {module_name}***\n")
+        time.sleep(3)
+        clear_output()
+        
     except subprocess.CalledProcessError:
-        print(f"Failed to install npm module: {module_name}")
+        print(f"\n***Failed to install npm module: {module_name}***\n")
+        sys.exit()
 
 
 def run_in_virtualenv(commands,selected_dir):
@@ -17,18 +30,30 @@ def run_in_virtualenv(commands,selected_dir):
         activate_cmd = '. .venv/bin/activate && '
         full_cmd = activate_cmd + ' && '.join(commands)
         subprocess.check_call(full_cmd, shell=True,cwd=selected_dir)
+        print(f"\n***Successfully virtual command: {commands}***\n")
+        time.sleep(3)
+        clear_output()
+
     except subprocess.CalledProcessError:
-        print("Failed to execute commands in the virtual environment.")
+        print("\n***Failed to execute commands in the virtual environment.***\n")
+        sys.exit()
     
+def check_installs():
+    try:
+        subprocess.check_call(['node', '-v'])
+        print("\n***NodeJs confirmed to be installed***\n")
+    except subprocess.CalledProcessError:
+        print("\n***NodeJs is not installed. Please install or try to install packages manually***\n")
+        sys.exit()
 
 
 current_file_path = os.path.abspath(__file__) ##project file destination
 
 dest_file_path = os.path.join(os.path.dirname(current_file_path), 'project/carhub')
-install_npm_module("react-scripts --save",dest_file_path)
+
+install_npm_module("react-scripts",dest_file_path,True)
 install_npm_module("@coreui/coreui",dest_file_path)
 install_npm_module("@coreui/react",dest_file_path)
-
 
 dest_file_path = os.path.join(os.path.dirname(current_file_path), 'backend')
 virtualenv_commands = [
@@ -36,4 +61,7 @@ virtualenv_commands = [
     'pip install djangorestframework',
     'pip install django-cors-headers'
 ]
+
 run_in_virtualenv(virtualenv_commands,dest_file_path)
+
+print("All packages for the website have been properly installed")
